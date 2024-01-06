8b7681c900e0ed757ed344c5a92304520045b084
Phattou <trinhxuanphat357@gmail.com>
2024-01-06 21:32:44 +0700

    OpenApp & AddToCart

--- App.js
+++ App.js
@@ -3,14 +3,15 @@ import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import OpenAppScreen from './Screens/OpenAppScreen';
-
+import AddToCart from './Screens/AddToCart';
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="OpenApp" component={OpenAppScreen} />
+        <Stack.Screen name="AddToCart" component={AddToCart} options={{headerShown:false}} />
+        <Stack.Screen name="OpenApp" component={OpenAppScreen} options={{headerShown:false}} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- Screens/OpenAppScreen.js
+++ Screens/OpenAppScreen.js
@@ -1,10 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Image,Pressable} from 'react-native';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 export default function OpenAppScreen() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View> 
+        <Image resizeMode='contain' style={{width:250,height:300}} source={require("../Images/image1.jpg")}/> 
+        </View>
+        <Text style={{fontWeight:'bold',fontSize:40}}> Keep reading,</Text>
+        <Text style={{fontWeight:'bold',fontSize:25}}> You'll fall in love</Text>
+        <Text style={{fontSize:15,paddingHorizontal:10,textAlign:'center',marginTop:50,fontWeight:"450"}}> A library of bite-sized business eBooks on soft skils and access to 30+ millions books in various languages with an easy and simple monthly subscription and read anywhere, any devices.</Text>
+        <Pressable>
+          <View style={{width:200,height:50,backgroundColor:"#F47575",marginTop:40, justifyContent:"center" }}> 
+          <Text style={{fontWeight:"bold",fontSize:18, textAlign:"center",}}> Start your journey </Text>
+          </View>          
+        </Pressable>
+        <Pressable style={{marginTop:30}}> <FontAwesome5 name="arrow-circle-right" size={50} color="#F47575" /> </Pressable>
+
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +28,5 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
   },
 });
